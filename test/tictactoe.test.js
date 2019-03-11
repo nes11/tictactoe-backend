8df80663d340c7dc832d2eeedb07cc1ba8b76ded
nes11 <agnesgaroux47@gmail.com>
2019-03-11 14:39:48 +0000

    API tests + function that checks for a winner

--- test/tictactoe.test.js
+++ test/tictactoe.test.js
@@ -1,17 +1,48 @@
 const { expect } = require('chai');
 const axios = require('axios');
 
-const testReqBody = {
-	"currentBoard": [null, null, null, null, "X", "O", null, null, null], 
-	"clickedSquareId": 2, 
-	"player": "O" 
-};
-
 describe('tictactoe', () => {
-  it('should give the right response', async () => {
+  it('returns the correct newBoard array', async () => {
+    const testReqBody = {
+      "currentBoard": [null, null, null, null, null, null, null, null, null], 
+      "clickedSquareId": 2, 
+      "player": "O" 
+    };
     const res = await axios.post('http://localhost:4000/api', testReqBody);
     expect(res.status).to.equal(200);
     expect(res.data).to.be.an('object');
+    expect(res.data.newBoard[2]).to.be.equal('O');
+  });
 
-  }); 
+  it("correctly alternates players", async () => {
+    const testReqBody = {
+      "currentBoard": ['O', null, null, 'X', null, null, null, null, null], 
+      "clickedSquareId": 1, 
+      "player": "O" 
+    };
+    const res = await axios.post('http://localhost:4000/api', testReqBody);
+    expect(res.status).to.equal(200);
+    expect(res.data).to.be.an('object');
+    expect(res.data).to.have.all.keys('newBoard', 'nextPlayer');
+    expect(res.data.newBoard[1]).to.be.equal('O');
+    expect(res.data.nextPlayer).to.be.equal('X');
+  })
+  
+  it('declares the winner accurately', async () => {
+    const testReqBody = {
+      "currentBoard": ['O', null, null, 'O', null, null, null, null, null], 
+      "clickedSquareId": 6, 
+      "player": "O" 
+    };
+    const res = await axios.post('http://localhost:4000/api', testReqBody);
+    expect(res.status).to.equal(200);
+    expect(res.data).to.be.an('object');
+    expect(res.data).to.have.all.keys('newBoard', 'result');
+    expect(res.data.newBoard).to.be.deep.equal(["O", null, null, "O", null, null, "O", null, null]);
+    expect(res.data.result).to.be.equal('O wins the game!');
+
+  })
 });
+
+
+
--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,3 @@
-// req needs => which square was clicked + which player 
-// res needs => new state of the board + next player 
-// for each request => check state of the board against winning combos
-//function takes in req object (index of the square + player)
-
 const updateBoard = (currentBoard, clickedSquareId, player) => {
   return currentBoard.map((el, i) => i === clickedSquareId ? player : el)
 }
@@ -11,32 +6,30 @@ const whoSNext = (player) => {
   return player === 'X' ? 'O' : 'X';
 }
 
+const isWinner = (newBoard) => {
+  const winningSquareCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4 ,7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  return winningSquareCombos.some(combo => {
+    const [a, b, c] = combo;
+    return newBoard[a] && newBoard[a] === newBoard[b] && newBoard[a] === newBoard[c];
+  });
+};
+
 const updateGame = (currentBoard, clickedSquareId, player) => {
-  if (currentBoard[clickedSquareId] === null) {
     const newBoard = updateBoard(currentBoard, clickedSquareId, player);
     const nextPlayer = whoSNext(player);
-    return { newBoard, nextPlayer };
-  } else {
-    return { currentBoard, player};
-  }
-}
-
-// const updateGameHistory = (newBoard) => {
-//   let gameHistory = [];
-//   gameHistory.concat(newBoard);
-//   return gameHistory;
-// }
-
-const winningCombo = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4 ,7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+    return isWinner(newBoard) 
+    ? { newBoard, result:`${player} wins the game!`}
+    : { newBoard, nextPlayer };
+};
 
 module.exports = {
   updateGame,
